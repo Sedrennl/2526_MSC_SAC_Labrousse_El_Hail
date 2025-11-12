7c31e5b91d88bc739fc14a20b297e6f735a6e5a4
Sedrennl <sedrenn.labrousse@gmail.com>
2025-11-12 17:28:06 +0100

    read me + debut PWM

--- software/Base/Core/Src/motor_control/motor.c
+++ software/Base/Core/Src/motor_control/motor.c
@@ -6,3 +6,10 @@
  */
 
 #include "motor_control/motor.h"
+
+// ------------------- Définitions -------------------
+#define PWM_FREQUENCY_HZ      20000U    // Fréquence PWM : 20 kHz
+#define PWM_RESOLUTION_BITS   10U       // Résolution minimum 10 bits
+#define PWM_DUTY_CYCLE_PERCENT 60U      // Pour les tests, rapport cyclique à 60%
+#define MIN_DEAD_TIME_NS      300U      // Temps mort minimum !!!!300 ns !!!!
+
